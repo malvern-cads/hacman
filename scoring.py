de070aad786d1b73574676d1a9f4952d7b3b6c66
Jake Walker <hi@jakew.me>
2020-03-03 18:08:51 +0000

    Generate scoreboard on save

--- scoring.py
+++ scoring.py
@@ -5,6 +5,7 @@ from jinja2 import Template
 # The maximum number of scores to show on the scoreboard
 scoreboard_limit = 30
 
+
 def load_scores():
     logger.debug("Loading scores...")
     score_file = "scores.json"
@@ -15,28 +16,6 @@ def load_scores():
         return scores
     except FileNotFoundError:
         return []
-
-
-def save_scores(new_scores):
-    logger.debug("Saving scores...")
-    score_file = "scores.json"
-    text = json.dumps(new_scores)
-    with open(score_file, "w") as out_file:
-        out_file.write(text)
-
-
-def add_score(name, score, time, school):
-    logger.debug("Adding score {} FROM {} - {} in {}s".format(name, school,
-                                                              score, time))
-    new_score = {
-            "name": name,
-            "score": score,
-            "time": time,
-            "school": school
-        }
-    current_scores = load_scores()
-    current_scores.append(new_score)
-    save_scores(current_scores)
 
 
 def get_sorted_scores():
@@ -60,6 +39,29 @@ def generate_web_page():
     # save webpage
     with open("scoring_report.html", "w") as out_file:
         out_file.write(score_page)
+
+
+def save_scores(new_scores):
+    logger.debug("Saving scores...")
+    score_file = "scores.json"
+    text = json.dumps(new_scores)
+    with open(score_file, "w") as out_file:
+        out_file.write(text)
+    generate_web_page()
+
+
+def add_score(name, score, time, school):
+    logger.debug("Adding score {} FROM {} - {} in {}s".format(name, school,
+                                                              score, time))
+    new_score = {
+            "name": name,
+            "score": score,
+            "time": time,
+            "school": school
+        }
+    current_scores = load_scores()
+    current_scores.append(new_score)
+    save_scores(current_scores)
 
 
 if __name__ == "__main__":

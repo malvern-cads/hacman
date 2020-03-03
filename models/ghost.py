06c2c3a1b0a86e5f99f4fee840c2d03450596a66
Jake Walker <hi@jakew.me>
2020-03-03 18:11:09 +0000

    Fix formatting

--- models/ghost.py
+++ models/ghost.py
@@ -33,38 +33,19 @@ class Ghost(Player):
         possible = self.get_junction(walls, cx, cy)
 
         if self.direction in possible and len(possible) >= 3:
-            # if self.name == "blinky":
-            #     print("change blink", self.direction)
-            #     best = None
-            #     shortest = 1000000
-            #     px = self.player.rect.left
-            #     py = self.player.rect.top
-            #     for direction in possible:
-            #         manhat = abs(cx + self.unit_direction * direction[0] - px) + abs(cy + self.unit_direction * direction[1] - py)
-            #         print(manhat)
-            #         if manhat < shortest:
-            #             shortest = manhat
-            #             best = direction
-            #     self.direction = best
-
-
-            # else:
-            #     self.direction = random.choice(possible)
             self.direction = random.choice(possible)
         # corner time
         elif self.direction not in possible and len(possible) == 2:
-            self.direction = [i for i in possible if abs(i[0]) != abs(self.direction[0])][0]
-
+            self.direction = [i for i in possible if abs(
+                i[0]) != abs(self.direction[0])][0]
 
         # if there is a wall then choose another direction
         if self.is_wall(walls, nx, ny):
             possible = self.get_junction(walls, cx, cy)
             self.direction = random.choice(possible)
 
-
         self.change_x = self.direction[0] * self.unit_direction
         self.change_y = self.direction[1] * self.unit_direction
-
 
     def get_junction(self, walls, cx, cy):
         possible = []
@@ -76,8 +57,6 @@ class Ghost(Player):
                 possible.append(direction)
 
         return possible
-
-
 
     def is_wall(self, walls, nx, ny):
         old_x = self.rect.left
